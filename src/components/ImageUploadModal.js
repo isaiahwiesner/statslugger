752851ab3e849d2dbe52f3e98e15842a149a6f49
BuildTools <unconfigured@null.spigotmc.org>
2023-04-25 11:22:54 -0400

    Updated images

--- src/components/ImageUploadModal.js
+++ src/components/ImageUploadModal.js
@@ -79,63 +79,65 @@ export default function ImageUploadModal({ open, onClose, onSuccess }) {
 
   return (
     <Modal open={open} onClose={!isLoading ? handleOnClose : null} sx={{ display: 'grid', placeItems: 'center' }}>
-      <Card sx={{ width: '100%', maxWidth: '30rem', position: 'relative' }}>
-        <Loading loading={isLoading} />
-        <Box sx={{ p: 2 }}>
-          <Typography variant="h3">Upload Image</Typography>
-        </Box>
-        <Divider />
-        <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1, alignItems: 'center' }}>
-          {selectFileError && <Alert severity="error" sx={{ width: '100%' }}>{selectFileError}</Alert>}
-          {error && <Alert severity="error" sx={{ width: '100%' }}>{error}</Alert>}
-          <label htmlFor="upload-btn">
-            <input
-              style={{ display: 'none' }}
-              id="upload-btn"
-              type="file"
-              onChange={handleSelectFile}
-              accept='.png,.jpeg,.jpg,.gif'
-              disabled={isLoading || photoURL}
-            />
-
-            <Button variant="outlined" component="span" disabled={isLoading || photoURL}>
-              <Add />
-            </Button>
-          </label>
-          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-            <Typography>{file ? file.name : 'Select File'}</Typography>
-            {file && (
-              <IconButton size="small" sx={{ width: 24, height: 24 }} onClick={handleResetFile}>
-              <Delete sx={{ width: 18, height: 18 }} />
-            </IconButton>
-            )}
+      <Box sx={{ width: '100%', maxWidth: '30rem', position: 'relative', p: { xs: 2, md: 0 } }}>
+        <Card sx={{ width: '100%' }}>
+          <Loading loading={isLoading} />
+          <Box sx={{ p: 2 }}>
+            <Typography variant="h3">Upload Image</Typography>
           </Box>
-        </Box>
-        <Divider>OR</Divider>
-        <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1, alignItems: 'center' }}>
-          {urlError && !urlErrorFields && <Alert severity="error" sx={{ width: '100%' }}>{urlError}</Alert>}
-          <TextField
-            type="text"
-            label="Paste image URL"
-            sx={{ width: '100%' }}
-            value={photoURL}
-            onChange={(e) => setPhotoURL(e.target.value)}
-            disabled={isLoading || base64 || file}
-            error={urlError}
-            helperText={urlErrorFields.includes('photoURL') && urlError}
-            InputProps={{
-              endAdornment: <IconButton size="small" sx={{ width: 24, height: 24 }} onClick={handleResetPhotoURL}>
-                {photoURL && <Clear sx={{ width: 18, height: 18 }} />}
-              </IconButton>
-            }}
-          />
-        </Box>
-        <Divider />
-        <Box sx={{ p: 2, display: 'flex', justifyContent: 'end', gap: 2 }}>
-          <Button variant="text" onClick={handleOnClose} disabled={isLoading}>Cancel</Button>
-          <Button variant="contained" onClick={handleUpload} disabled={isLoading || (!base64 && !photoURL)}>Upload</Button>
-        </Box>
-      </Card>
+          <Divider />
+          <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1, alignItems: 'center' }}>
+            {selectFileError && <Alert severity="error" sx={{ width: '100%' }}>{selectFileError}</Alert>}
+            {error && <Alert severity="error" sx={{ width: '100%' }}>{error}</Alert>}
+            <label htmlFor="upload-btn">
+              <input
+                style={{ display: 'none' }}
+                id="upload-btn"
+                type="file"
+                onChange={handleSelectFile}
+                accept='.png,.jpeg,.jpg,.gif'
+                disabled={isLoading || photoURL}
+              />
+
+              <Button variant="outlined" component="span" disabled={isLoading || photoURL}>
+                <Add />
+              </Button>
+            </label>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <Typography>{file ? file.name : 'Select File'}</Typography>
+              {file && (
+                <IconButton size="small" sx={{ width: 24, height: 24 }} onClick={handleResetFile}>
+                  <Delete sx={{ width: 18, height: 18 }} />
+                </IconButton>
+              )}
+            </Box>
+          </Box>
+          <Divider>OR</Divider>
+          <Box sx={{ p: 2, display: 'flex', flexDirection: 'column', gap: 1, alignItems: 'center' }}>
+            {urlError && !urlErrorFields && <Alert severity="error" sx={{ width: '100%' }}>{urlError}</Alert>}
+            <TextField
+              type="text"
+              label="Paste image URL"
+              sx={{ width: '100%' }}
+              value={photoURL}
+              onChange={(e) => setPhotoURL(e.target.value)}
+              disabled={isLoading || base64 || file}
+              error={urlError}
+              helperText={urlErrorFields.includes('photoURL') && urlError}
+              InputProps={{
+                endAdornment: <IconButton size="small" sx={{ width: 24, height: 24 }} onClick={handleResetPhotoURL}>
+                  {photoURL && <Clear sx={{ width: 18, height: 18 }} />}
+                </IconButton>
+              }}
+            />
+          </Box>
+          <Divider />
+          <Box sx={{ p: 2, display: 'flex', justifyContent: 'end', gap: 2 }}>
+            <Button variant="text" onClick={handleOnClose} disabled={isLoading}>Cancel</Button>
+            <Button variant="contained" onClick={handleUpload} disabled={isLoading || (!base64 && !photoURL)}>Upload</Button>
+          </Box>
+        </Card>
+      </Box>
     </Modal>
   )
 }
